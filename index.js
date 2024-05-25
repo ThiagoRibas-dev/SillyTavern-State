e3253c012574b07ed03743647b55b2d1793cc6c3
Thiago Silva Ribas <thiagoribas91@hotmail.com>
2024-05-24 23:05:19 -0300

    Adjusted index

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-import { chat_metadata, callPopup, saveSettingsDebounced, is_send_press } from '../../../../script.js';
-import { getContext, extension_settings, saveMetadataDebounced } from '../../../extensions.js';
-import {
-    substituteParams,
-    eventSource,
-    event_types,
-    generateQuietPrompt,
-} from '../../../../script.js';
-import { registerSlashCommand } from '../../../slash-commands.js';
-import { waitUntilCondition } from '../../../utils.js';
-import { is_group_generating, selected_group } from '../../../group-chats.js';
+// The main script for the extension
+// The following are examples of some basic extension functionality
 
-const MODULE_NAME = 'PromptEvaluator';
+//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
+import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+
+//You'll likely need to import some other functions from the main script
+import { saveSettingsDebounced } from "../../../../script.js";
+
+import {
+  eventSource,
+  event_types,
+  generateQuietPrompt, saveSettingsDebounced, substituteParams
+} from '../../../../script.js';
+import { getContext } from '../../../extensions.js';
+
+const MODULE_NAME = 'StatePrompts';
+// Keep track of where your extension is located, name should match repo name
+const extensionName = "st-extension-state";
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {};
 
 let records = [];
 let lastSystemMessages = {};
@@ -119,30 +127,6 @@ function saveState() {
 }
 
 //###############################//
-//#         UI Elements         #//
-//###############################//
-
-const promptEvaluatorPanel = `
-<div id="prompt-evaluator-panel">
-    <h3>Prompt Evaluator</h3>
-    <div>
-        <label for="prompt-evaluator-prompt">Prompt:</label>
-        <input type="text" id="prompt-evaluator-prompt">
-    </div>
-    <div>
-        <label for="prompt-evaluator-message-count">Message Count:</label>
-        <input type="number" id="prompt-evaluator-message-count" min="1">
-    </div>
-    <button id="add-prompt-record">Add Record</button>
-    <ul id="prompt-record-list"></ul>
-    <textarea id="prompt-evaluator-save-state" style="display: none;"></textarea>
-</div>
-`;
-
-// Append the panel to the UI
-$('body').append(promptEvaluatorPanel);
-
-//###############################//
 //#       UI Event Listeners     #//
 //###############################//
 
@@ -152,7 +136,45 @@ $('#add-prompt-record').on('click', () => {
     addRecord(prompt, messageCount);
 });
 
-// Initialize
-$(document).ready(() => {
-    loadState();
+ 
+// Loads the extension settings if they exist, otherwise initializes them to the defaults.
+async function loadSettings() {
+  //Create the settings if they don't exist
+  extension_settings[extensionName] = extension_settings[extensionName] || {};
+  if (Object.keys(extension_settings[extensionName]).length === 0) {
+    Object.assign(extension_settings[extensionName], defaultSettings);
+  }
+
+  // Updating settings in the UI
+  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+}
+
+jQuery(async () => {
+  //###############################//
+  //#         UI Elements         #//
+  //###############################//
+  const settingsHtml = `
+  <div id="prompt-evaluator-panel">
+      <h3>Prompt Evaluator</h3>
+      <div>
+          <label for="prompt-evaluator-prompt">Prompt:</label>
+          <input type="text" id="prompt-evaluator-prompt">
+      </div>
+      <div>
+          <label for="prompt-evaluator-message-count">Message Count:</label>
+          <input type="number" id="prompt-evaluator-message-count" min="1">
+      </div>
+      <button id="add-prompt-record">Add Record</button>
+      <ul id="prompt-record-list"></ul>
+      <textarea id="prompt-evaluator-save-state" style="display: none;"></textarea>
+  </div>
+  `;
+  
+  // Append settingsHtml to extensions_settings
+  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
+  // Left should be extensions that deal with system functions and right should be visual/UI related 
+  $("#extensions_settings").append(settingsHtml);
+
+  // Load settings when starting things up (if you have any)
+  loadSettings();
 });
