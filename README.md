aede766ec01fb6b917dc382bb4c222330fb58fe0
Thiago Silva Ribas <thiagoribas91@hotmail.com>
2024-05-30 13:12:11 -0300

    Adittion of buttons to the UI to send the prompts manually.

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Prompt persistent state from the AI.
 The extension allows the user to configure a number of prompts that are automatically sent after the AI's response to the User's prompt, adding the result of each prompt as an individual message to the chat, as a form of persistent context.
 TODO:
- - Change the configuration so that it's saved for each Character instead of each individual Chat;
  - Add an insertion template field;
- - Implement a keyword system, where the User can assign keywords to each prompt and those prompts will only trigger in case the last N messages contain said keywords (like a lorebook);
- - Add an option to choose between adding the results of each prompt as individual messages (as is now) and appending the results to the character's last message;
- - Add an easy way to run an specific state prompt or all state prompts, manually, either by adding a toolbar to the UI or by adding a temporary slash command;
- - Fix the UI;
+ - Add an option to make the result of a prompt unique in the chat, as in, before adding a new result, the previous one will be removed;
  - Different event triggers (After User message, before User Message, before Assistant Message, etc);
- - Add a mini prompt checkbox to the UI;
+ ~~- Add an easy way to run a specific state prompt or all state prompts, manually, either by adding a toolbar to the UI or by adding a temporary slash command (like the quick replies extension);~~(DONE)
+ - Change the configuration so that it's saved for each Character instead of each individual Chat;
+ - Add an option to choose between adding the results of each prompt as individual messages (as is now) and appending the results to character's last message;
+ - Add a mini prompt checkbox to the UI to decide how the result of a given prompt is shown in the chat history;
+ - Fix the UI;
+ - Implement a keyword system, where the User can assign keywords to each prompt and those prompts will only trigger in case the last N messages contain said keywords (like a lorebook);
--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-import { eventSource, event_types, saveSettingsDebounced, saveChatConditional } from "../../../../script.js";
+import { eventSource, event_types, saveChatConditional, saveSettingsDebounced } from "../../../../script.js";
 import { extension_settings, getContext } from "../../../extensions.js";
 export { MODULE_NAME };
 
-const extensionName = "SillyTavern-State";
-const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+const EXTENSION_NAME = "SillyTavern-State";
+const EXTENSION_FOLDER_PATH = `scripts/extensions/third-party/${EXTENSION_NAME}`;
 const MODULE_NAME = 'State';
 const DEBUG_PREFIX = "<State extension> ";
+const CONTEXT = getContext();
+
 let CHAT_ID = null;
+let IS_LOADING = true;
+let IS_GENERATING = false;
 
 //#############################//
 //  Extension UI and Settings  //
 //#############################//
 function loadSettings(chatId) {
+    IS_LOADING = true;
     console.log(DEBUG_PREFIX, `Loading ${chatId}`);
-    
+
     const li = $('#state-prompt-set');
     const se = $("#state_enabled");
     const add = $("#sp--set-new");
@@ -23,7 +28,6 @@ function loadSettings(chatId) {
     li.html('');
     se.prop('checked', false);
     lbl.empty();
-
     if (!extension_settings[MODULE_NAME][chatId]) {
         extension_settings[MODULE_NAME][chatId] = {
             enabled: false,
@@ -37,23 +41,52 @@ function loadSettings(chatId) {
 
     CHAT_ID = chatId;
 
-    se.prop('checked', extension_settings[MODULE_NAME][chatId].enabled);
-    se.on("click", () => { onEnabledClick(chatId) });
-    add.on("click", () => { onAddNew(li, chatId); });
+    const chatSettings = extension_settings[MODULE_NAME][chatId];
+    const btns = $(`<div id="sp_container" style="z-index: 99999; /*background: red;*/ position:fixed; bottom: 0; margin-bottom: 5vh; margin-left: 1vh;"></div>`)
+
+    $('#chat').append(btns);
+    se.prop('checked', chatSettings.enabled);
+    se.on("click", () => { onEnabledClick(chatSettings) });
+    add.on("click", () => { onAddNew(li, btns, chatSettings); updatePromptButtons(btns, chatSettings); });
     lbl.text(`Prompts for chat "${chatId}"`);
 
-    const prompts = extension_settings[MODULE_NAME][chatId].prompts;
+    const prompts = chatSettings.prompts;
     for (var k in prompts) {
-        onAddNew(li, chatId, prompts[k]);
+        onAddNew(li, btns, chatSettings, prompts[k]);
+    }
+
+    updatePromptButtons(btns, chatSettings);
+}
+
+function updatePromptButtons(btns, chatSettings) {
+    console.log(DEBUG_PREFIX, 'Updating prompt btns.')
+    btns.empty();
+    const statePrompts = chatSettings.prompts;
+    for (var k in statePrompts) {
+        const value = statePrompts[k];
+        const divBtn = getBtn(k, value);
+        btns.append(divBtn);
     }
 }
 
-async function onEnabledClick(chatId) {
-    extension_settings[MODULE_NAME][chatId].enabled = $('#state_enabled').is(':checked');
+function getBtn(k, value) {
+    const vlCount = parseInt(k)+1;
+    const elBtn = $(`<a title="${value}" style="text-shadow: rgb(0, 0, 0) 0px 0px 2px; box-sizing: border-box; color: rgb(255, 255, 255); color-scheme: light only; cursor: pointer; font-family: "Noto Sans", "Noto Color Emoji", sans-serif; font-size: 16.5px; font-weight: 400; vertical-align: middle;">${vlCount}-${value[0].toUpperCase()}</a>`);
+    const divBtn = $(`<div style="border: 1px black solid; border-radius: 4px;"><br></div>`);
+    divBtn.append(elBtn);
+    elBtn.on('click', () => {
+        console.log(DEBUG_PREFIX, 'CLICKED', value);
+        sendPrompt(value, k);
+    });
+    return divBtn;
+}
+
+async function onEnabledClick(chatSettings) {
+    chatSettings.enabled = $('#state_enabled').is(':checked');
     saveSettingsDebounced();
 }
 
-async function savePrompt(chatId) {
+async function savePrompt(chatSettings) {
     const prompts = [];
     const values = $('.state-prompt-area').toArray();
     for (var k in values) {
@@ -63,24 +96,44 @@ async function savePrompt(chatId) {
         }
     }
 
-    extension_settings[MODULE_NAME][chatId].prompts = prompts;
+    chatSettings.prompts = prompts;
     saveSettingsDebounced();
 }
 
-async function removePrompt(elId) {
-    $(elId).remove();
-    saveSettingsDebounced();
-}
+async function onAddNew(li, btns, chatSettings, value = '') {
+    const promptArea = $(`<textarea class="state-prompt-area" placeholder="(Prompt sent to probe the current state)" class="text_pole widthUnset flex1" rows="2">${value}</textarea>`);
+    const rmvBtn = $(`<div class="menu_button menu_button_icon fa-solid fa-trash-can redWarningBG" title="Remove"></div>`);
+    const liEl = $(`<li style="width: 100%;"></li>`)
 
-async function onAddNew(li, chatId, value = '') {
-    const count = li.children().length;
-    li.append(`<li id="${count}-state-area-li" style="width: 100%;"><textarea id="${count}-state-area" class="state-prompt-area" placeholder="(Prompt sent to probe the current state)" class="text_pole widthUnset flex1" rows="2">${value}</textarea><div id="${count}-state-area-remove" class="menu_button menu_button_icon fa-solid fa-trash-can redWarningBG" title="Remove"></div></li>`);
+    promptArea.on('change', () => {
+        savePrompt(chatSettings);
+        updatePromptButtons(btns, chatSettings);
+    });
+    rmvBtn.on('click', () => {
+        liEl.remove();
+        savePrompt(chatSettings);
+        updatePromptButtons(btns, chatSettings);
+        saveSettingsDebounced();
+    });
 
-    $(`#${count}-state-area`).on('change', () => { savePrompt(chatId); });
-    $(`#${count}-state-area-remove`).on('click', () => { removePrompt(`#${count}-state-area-li`); });
+    liEl.append(promptArea);
+    liEl.append(rmvBtn);
+    li.append(liEl);
 }
 
 async function processStateText() {
+    if (IS_LOADING) {
+        IS_LOADING = false;
+        return;
+    }
+
+    if (IS_GENERATING) {
+        toastr.error("Generation already in process.");
+        return;
+    }
+
+    IS_GENERATING = true;
+
     const chatId = CHAT_ID;
     console.debug(DEBUG_PREFIX, 'processStateText', chatId);
 
@@ -88,29 +141,41 @@ async function processStateText() {
         return;
 
     try {
-        const context = getContext();
         const prompts = extension_settings[MODULE_NAME][chatId].prompts;
-        const chat = getContext().chat;
         for (var k in prompts) {
             const prmpt = prompts[k];
-            if (prmpt) {
-                toastr.info(`State Extension. Sending prompt : ${prmpt}`);
-
-                const id = `State prompt ${k}`;
-                // removeObjectFromArray(chat, "state_extension_id", id);
-
-                const resp = await context.generateQuietPrompt(prmpt);
-                const message = { "name": "System", "is_user": false, "is_system": false, "state_extension_id": id, "send_date": new Date().toString(), "mes": resp, "extra": { "isSmallSys": true } };
-                console.log(DEBUG_PREFIX, 'Adding Message', message);
-                chat.push(message);
-                context.addOneMessage(message);
-                await eventSource.emit(event_types.CHARACTER_MESSAGE_RENDERED, (chat.length - 1));
-                await saveChatConditional();
-            }
+            await sendPrompt(prmpt, k);
         }
     } catch (error) {
         toastr.error("State extension: Error during generation.", error);
     }
+    IS_GENERATING = false;
+}
+
+async function sendPrompt(prmpt, k) {
+    if (prmpt) {
+        toastr.info(`State Extension. Sending prompt : ${prmpt}`);
+
+        const chat = CONTEXT.chat;
+        const id = `State prompt ${k}`;
+
+        // await removeObjectFromChat(chat, id);
+
+        const resp = await CONTEXT.generateQuietPrompt(prmpt);
+        const message = { "name": "System", "is_user": false, "is_system": false, "state_extension_id": id, "send_date": new Date().toString(), "mes": resp, "extra": { "isSmallSys": true } };
+        console.log(DEBUG_PREFIX, 'Adding Message', message);
+
+        chat.push(message);
+        CONTEXT.addOneMessage(message);
+        await eventSource.emit(event_types.CHARACTER_MESSAGE_RENDERED, (chat.length - 1));
+        await saveChatConditional();
+    }
+}
+
+async function removeObjectFromChat(chat, id) {
+    removeObjectFromArray(chat, "state_extension_id", id);
+    await eventSource.emit(event_types.CHARACTER_MESSAGE_RENDERED, (chat.length - 1));
+    await saveChatConditional();
 }
 
 function removeObjectFromArray(array, key, value) {
@@ -156,10 +221,8 @@ async function getDate(now) {
 //#############################//
 jQuery(async () => {
     console.log(DEBUG_PREFIX, 'loading', extension_settings[MODULE_NAME]);
-
-    const windowHtml = $(await $.get(`${extensionFolderPath}/window.html`));
+    const windowHtml = $(await $.get(`${EXTENSION_FOLDER_PATH}/window.html`));
     $('#extensions_settings').append(windowHtml);
-
     eventSource.on(event_types.CHAT_CHANGED, (chatId) => loadSettings(chatId));
     eventSource.on(event_types.MESSAGE_RECEIVED, processStateText);
 });
